82f5fe2f19a2967e87ade1cfec985a38d90aa5df
Nicoly001 <nicolyrod43@gmail.com>
2022-06-16 16:44:33 -0300

    bug deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Routes from './routes'
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes />
-    </Router>
+    </HashRouter>
+
   );
 }
 
