2bc5827ec6e2d083a145465843c26fdc18f0b5ae
Nicoly001 <nicolyrod43@gmail.com>
2022-06-18 17:17:27 -0300

    Corrigir nomes cientificos

--- src/pages/Perguntas/index.jsx
+++ src/pages/Perguntas/index.jsx
@@ -15,10 +15,10 @@ function Perguntas() {
             id: 1,
             descricao: 'A cigarrinha é responsável por succionar a seiva das plantas do milho e ser vetora de vírus e molicutes que causam os enfezamentos no milho.',
             questao: 'Qual é a forma correta do nome científico desse inseto-praga?',
-            a: 'D. Maidis',
-            b: 'Dalbulus maidis - Dalbulus maidis (DeLong & Wolcott) (Hemiptera: Cicadellidae)',
-            c: 'Dalbulus maidis maidis',
-            d: 'Dalbulus sp. - Dalbulus maidis (DeLong & Wolcott) (Hymenoptera: Cicadellidae)',
+            a: <i>D. Maidis</i>,
+            b: <><i>Dalbulus maidis - Dalbulus maidis</i> (DeLong & Wolcott) (Hemiptera: Cicadellidae)</>,
+            c: <i>Dalbulus maidis maidis</i>,
+            d: <><i>Dalbulus sp. - Dalbulus maidis </i> (DeLong & Wolcott) (Hymenoptera: Cicadellidae)</>,
             correta: 'b',
             respondido: false,
             imagens: {
@@ -30,29 +30,29 @@ function Perguntas() {
         },
         {
             id: 2,
-            descricao: 'A Diabrotica speciosa (Germar, 1824), conhecida como vaquinha, é um besouro pequeno e muito marcante, de coloração verde e amarela. Também é conhecida por brasileirinho, cascudinho ou patriota, e suas larvas são conhecidas como larva-alfinete. É uma praga polífaga e a fase jovem é subterrânea.',
+            descricao: <>A <i>Diabrotica speciosa</i> (Germar, 1824), conhecida como vaquinha, é um besouro pequeno e muito marcante, de coloração verde e amarela. Também é conhecida por brasileirinho, cascudinho ou patriota, e suas larvas são conhecidas como larva-alfinete. É uma praga polífaga e a fase jovem é subterrânea.</>,
             questao: 'Qual a ordem e família deste inseto-praga?',
-            a: 'Ordem Coleoptera; Família Chrysomelidae',
-            b: 'Ordem Coleoptera; Família Lepidoptera',
-            c: 'Ordem Diptera; Família Chrysomelidae',
-            d: 'Ordem Lepidoptera; Família Chrysomelidae',
+            a: <>Ordem Coleoptera; Família Chrysomelidae</>,
+            b: <>Ordem Coleoptera; Família Lepidoptera</>,
+            c: <>Ordem Diptera; Família Chrysomelidae</>,
+            d: <>Ordem Lepidoptera; Família Chrysomelidae</>,
             correta: 'a',
             respondido: false,
             imagens: {
                 primeira: require('../../assets/images/diabrotica21.jpg'),
                 segunda: require('../../assets/images/diabrotica22.jpg'),
-                primeiraLegenda: 'Diabrotica speciosa - Fonte: Revista Cultivar',
-                segundaLegenda: 'Diabrotica speciosa - Fonte: Elevagro',
+                primeiraLegenda: <><i>Diabrotica speciosa</i> - Fonte: Revista Cultivar</>,
+                segundaLegenda: <><i>Diabrotica speciosa</i> - Fonte: Elevagro</>,
             }
         },
         {
             id: 3,
-            descricao: 'O percevejo-barriga-verde Diceraeus furcatus (Fabricius, 1775), é um inseto sugador, que age a partir da introdução do estilete no tecido das plantas, que se tornam seu alimento e fonte para o desenvolvimento. Os danos ocorrem, principalmente, nos estádios iniciais da cultura, com ataque no colo das plântulas causando lesões nas folhas, redução de estande e perdas em produtividade.',
+            descricao: <>O percevejo-barriga-verde <i>Diceraeus furcatus</i> (Fabricius, 1775), é um inseto sugador, que age a partir da introdução do estilete no tecido das plantas, que se tornam seu alimento e fonte para o desenvolvimento. Os danos ocorrem, principalmente, nos estádios iniciais da cultura, com ataque no colo das plântulas causando lesões nas folhas, redução de estande e perdas em produtividade.</>,
             questao: 'Qual a ordem e família do percevejo-barriga-verde?',
-            a: 'Ordem Hemiptera; Família Cydnidae',
-            b: 'Ordem Hemiptera; Família Pentatomidae',
-            c: 'Ordem Coleoptera; Família Pentatomidae',
-            d: 'Ordem Coleoptera; Família Cicadellidae',
+            a: <>Ordem Hemiptera; Família Cydnidae</>,
+            b: <>Ordem Hemiptera; Família Pentatomidae</>,
+            c: <>Ordem Coleoptera; Família Pentatomidae</>,
+            d: <>Ordem Coleoptera; Família Cicadellidae</>,
             correta: 'b',
             respondido: false,
             imagens: {
@@ -66,10 +66,10 @@ function Perguntas() {
             id: 4,
             descricao: 'Ambas as espécies do percevejo barriga verde possuem a face dorsal marrom e a ventral verde. O percevejo A é ligeiramente maior, medindo cerca de 10 mm de comprimento, os prolongamentos laterais no pronoto, em forma de espinhos, da mesma cor do dorso. O percevejo B é menor (7 mm) e apresenta a extremidade dos espinhos mais escura do que o resto do dorso.',
             questao: 'O Percevejo A e B citados acima são, respectivamente:',
-            a: 'A - Diceraeus furcatus – Diceraeus furcatus (Fabricius, 1775) (Hemiptera: Pentatomidae) B - Diceraeus melacanthus – Diceraeus melacanthus (Dallas, 1851) (Hemiptera: Pentatomidae)',
-            b: 'A - Diceraeus melacanthus – Diceraeus melacanthus (Dallas, 1851) (Hemiptera: Pentatomidae) B - Diceraeus furcatus (Fabricius) (Hemiptera: Pentatomidae)',
-            c: 'A - Diceraeus melacanthus (Dallas, 1851) (Hemiptera: Pentatomidae) B - Dalbulus maidis (DeLong & Wolcott) (Hymenoptera: Cicadellidae)',
-            d: 'A - Dalbulus maidis – Dalbulus maidis (DeLong & Wolcott) (Hymenoptera: Cicadellidae) B - Diceraeus melacanthus (Dallas, 1851)',
+            a: <>A - <i>Diceraeus furcatus – Diceraeus furcatus</i> (Fabricius, 1775) (Hemiptera: Pentatomidae) B - <i>Diceraeus melacanthus – Diceraeus melacanthus</i> (Dallas, 1851) (Hemiptera: Pentatomidae)</>,
+            b: <>A - <i>Diceraeus melacanthus – Diceraeus melacanthus</i> (Dallas, 1851) (Hemiptera: Pentatomidae) B - <i>Diceraeus furcatus</i> (Fabricius) (Hemiptera: Pentatomidae)</>,
+            c: <>A - <i>Diceraeus melacanthus</i> (Dallas, 1851) (Hemiptera: Pentatomidae) B - <i>Dalbulus maidis</i> (DeLong & Wolcott) (Hymenoptera: Cicadellidae)</>,
+            d: <>A - <i>Dalbulus maidis – Dalbulus maidis</i> (DeLong & Wolcott) (Hymenoptera: Cicadellidae) B - <i>Diceraeus melacanthus</i> (Dallas, 1851)</>,
             correta: 'a',
             respondido: false,
             imagens: {
@@ -81,10 +81,10 @@ function Perguntas() {
             id: 5,
             descricao: 'O Percevejo marrom pode atacar ramos e hastes, mas o maior prejuízo ocorre quando atacam vagens em formação, ocasionando má formação de grãos, chamados de “grãos chochos” e consequente redução de produtividade. A cultura da soja é a principal hospedeira do percevejo-marrom.',
             questao: 'Qual o nome científico do Percevejo-marrom?',
-            a: 'Diceraeus melacanthus – Diceraeus melacanthus (Dallas, 1851) (Hemiptera: Pentatomidae)',
-            b: 'Euschistus spp',
-            c: 'Euschistus heros (Fabricius, 1798) (Hemiptera: Pentatomidae) ',
-            d: 'Euschistus Heros (Fabricius, 1798) (Hemiptera: Pentatomidae)',
+            a: <><i>Diceraeus melacanthus – Diceraeus melacanthus</i> (Dallas, 1851) (Hemiptera: Pentatomidae)</>,
+            b: <i>Euschistus spp</i>,
+            c: <><i>Euschistus heros</i> (Fabricius, 1798) (Hemiptera: Pentatomidae)</>,
+            d: <><i>Euschistus Heros</i> (Fabricius, 1798) (Hemiptera: Pentatomidae)</>,
             correta: 'c',
             respondido: false,
             imagens: {
@@ -94,12 +94,12 @@ function Perguntas() {
         },
         {
             id: 6,
-            descricao: 'O Percevejo-castanho Scaptocoris castanea (Perty, 1830) tem hábito subterrâneo e ataca as raízes, inserindo o aparelho sugador no tecido da planta, no qual se alimenta da seiva. Ao atacar as raízes, esses insetos injetam toxinas que reduzem o crescimento da planta, além de provocarem enfraquecimento, amarelecimento e murcha. Essa praga ataca culturas como soja, milho, sorgo, algodão, café, dentre outras.',
+            descricao: <>O Percevejo-castanho <i>Scaptocoris castanea</i> (Perty, 1830) tem hábito subterrâneo e ataca as raízes, inserindo o aparelho sugador no tecido da planta, no qual se alimenta da seiva. Ao atacar as raízes, esses insetos injetam toxinas que reduzem o crescimento da planta, além de provocarem enfraquecimento, amarelecimento e murcha. Essa praga ataca culturas como soja, milho, sorgo, algodão, café, dentre outras.</>,
             questao: 'Qual a ordem e família deste inseto-praga?',
-            a: 'Ordem Hemiptera; Família Cicadellidae',
-            b: 'Ordem Heteroptera; Família Lepidoptera',
-            c: 'Ordem Coleoptera; Família Cydnidae',
-            d: 'Ordem Hemipetera; Família Cydnidae',
+            a: <>Ordem Hemiptera; Família Cicadellidae</>,
+            b: <>Ordem Heteroptera; Família Lepidoptera</>,
+            c: <>Ordem Coleoptera; Família Cydnidae</>,
+            d: <>Ordem Hemipetera; Família Cydnidae</>,
             correta: 'd',
             respondido: false,
             imagens: {
@@ -113,10 +113,10 @@ function Perguntas() {
             id: 7,
             descricao: 'Algumas espécies de lagartas pertencentes ao gênero Spodoptera possuem maior preferência pela cultura da soja, atacando as plantas principalmente na fase reprodutiva. Observe as características descritas: Imagem 1 - Listras de cor creme, uma das faixas é interrompida por uma mancha escura que não chega até a cabeça. Imagem 2 - Listras amarelas no dorso que chegam até a cabeça, pontuações brancas e triângulos pretos.',
             questao: 'As pragas citadas acima são, respectivamente:',
-            a: 'Spodoptera cosmioides – Spodoptera cosmioides (Walker, 1858) (Lepidoptera: Noctuidae) e Spodoptera eridania – Spodoptera eridania (Cramer, 1782) (Lepidoptera: Noctuidae)',
-            b: 'Spodoptera eridania – Spodoptera eridania (Cramer, 1782) (Lepidoptera: Noctuidae) e Spodoptera cosmioides – Spodoptera cosmioides (Walker, 1858) (Lepidoptera: Noctuidae)',
-            c: 'Spodoptera frugiperda – Spodoptera frugiperda (J. E. Smith, 1797) (Lepidoptera: Noctuidae) e Spodoptera cosmioides – Spodoptera cosmioides (Walker, 1858) (Lepidoptera: Noctuidae)',
-            d: 'Spodoptera eridania – Spodoptera eridania (Cramer, 1782) (Lepidoptera: Noctuidae) e Spodoptera frugiperda – Spodoptera frugiperda (J. E. Smith, 1797) (Lepidoptera: Noctuidae)',
+            a: <><i>Spodoptera cosmioides – Spodoptera cosmioides</i> (Walker, 1858) (Lepidoptera: Noctuidae) e <i>Spodoptera eridania – Spodoptera eridania</i> (Cramer, 1782) (Lepidoptera: Noctuidae)</>,
+            b: <><i>Spodoptera eridania – Spodoptera eridania</i> (Cramer, 1782) (Lepidoptera: Noctuidae) e <i>Spodoptera cosmioides – Spodoptera cosmioides</i> (Walker, 1858) (Lepidoptera: Noctuidae)</>,
+            c: <><i>Spodoptera frugiperda – Spodoptera frugiperda</i> (J. E. Smith, 1797) (Lepidoptera: Noctuidae) e <i>Spodoptera cosmioides – Spodoptera cosmioides</i> (Walker, 1858) (Lepidoptera: Noctuidae)</>,
+            d: <><i>Spodoptera eridania – Spodoptera eridania</i> (Cramer, 1782) (Lepidoptera: Noctuidae) e <i>Spodoptera frugiperda – Spodoptera frugiperda</i> (J. E. Smith, 1797) (Lepidoptera: Noctuidae)</>,
             correta: 'b',
             respondido: false,
             imagens: {
@@ -130,10 +130,10 @@ function Perguntas() {
             id: 8,
             descricao: 'A lagarta-do-cartucho ou lagarta-militar, é uma das principais pragas do milho. Ela se alimenta tanto de órgãos vegetativos quanto de órgãos reprodutivos das plantas. Problemas com esta praga foram se agravando devido às modificações no sistema de produção, tais como cultivos sucessivos de plantas hospedeiras da praga conhecidas como “pontes verdes”, permitindo sua sobrevivência nas áreas de produção. Esse sistema de cultivo também aumentou a frequência de pulverização de inseticidas acarretando a seleção de populações resistentes.',
             questao: 'Qual o nome científico da lagarta-do-cartucho?',
-            a: 'Spodoptera frugiperda – Spodoptera frugiperda (J. E. Smith, 1797) (Lepidoptera: Noctuidae)',
-            b: 'Spodoptera frugiperda sp. (J. E. Smith, 1797) (Lepidoptera: Noctuidae)',
-            c: 'Spodoptera eridania – Spodoptera eridania (Cramer, 1782) (Lepidoptera: Noctuidae)',
-            d: 'Spodoptera e. spp (Cramer)',
+            a: <><i>Spodoptera frugiperda – Spodoptera frugiperda</i> (J. E. Smith, 1797) (Lepidoptera: Noctuidae)</>,
+            b: <><i>Spodoptera frugiperda sp.</i> (J. E. Smith, 1797) (Lepidoptera: Noctuidae)</>,
+            c: <><i>Spodoptera eridania – Spodoptera eridania</i> (Cramer, 1782) (Lepidoptera: Noctuidae)</>,
+            d: <><i>Spodoptera e. spp</i> (Cramer)</>,
             correta: 'a',
             respondido: false,
             imagens: {
@@ -147,10 +147,10 @@ function Perguntas() {
             id: 9,
             descricao: 'Trata-se de um lepidóptero polífago que se alimenta, principalmente, de folhas e frutos, podendo ocasionar graves danos econômicos. Esta praga ataca principalmente a cultura da soja e provoca danos como desfolha nos terços superiores da planta e as infestações ocorrem em reboleiras.',
             questao: 'Trata-se de:',
-            a: 'Spodoptera eridania – Spodoptera eridania (Cramer, 1782) (Lepidoptera: Noctuidae)',
-            b: 'Spodoptera frugiperda – Spodoptera frugiperda (J. E. Smith, 1797) (Lepidoptera: Noctuidae)',
-            c: 'Spodoptera albula – Spodoptera albula (Walker, 1857) (Lepidoptera: Noctuidae)',
-            d: 'Spodoptera Albula (Lepidoptera: Noctuidae)',
+            a: <><i>Spodoptera eridania – Spodoptera eridania</i> (Cramer, 1782) (Lepidoptera: Noctuidae)</>,
+            b: <><i>Spodoptera frugiperda – Spodoptera frugiperda</i> (J. E. Smith, 1797) (Lepidoptera: Noctuidae)</>,
+            c: <><i>Spodoptera albula – Spodoptera albula</i> (Walker, 1857) (Lepidoptera: Noctuidae)</>,
+            d: <><i>Spodoptera Albula</i> (Lepidoptera: Noctuidae)</>,
             correta: 'c',
             respondido: false,
             imagens: {
@@ -164,10 +164,10 @@ function Perguntas() {
             id: 10,
             descricao: 'As lagartas falsas-medideiras estão entre as principais pragas na cultura da soja. Normalmente a praga tem preferência por atacar a cultura nos estádios reprodutivos, mas pode também ser observada na fase final do desenvolvimento vegetativo. Dentre os principais danos, podemos destacar o consumo das folhas de soja. As lagartas consomem o parênquima foliar deixando as nervuras, conferindo aos folíolos aspecto rendilhado.',
             questao: '“A” e “B” são, respectivamente:',
-            a: 'Spodoptera eridania – Spodoptera eridania (Cramer, 1782) (Lepidoptera: Noctuidae) e Spodoptera cosmioides – Spodoptera cosmioides (Walker, 1858) (Lepidoptera: Noctuidae)',
-            b: 'Chrysodeixis includens – Chrysodeixis includens (Walker, 1858) (Lepidoptera: Noctuidae) e Rachiplusia nu – Rachiplusia nu (Guenée) (Lepidoptera: Noctuidae)',
-            c: 'Spodoptera cosmioides – Spodoptera cosmioides (Walker, 1858) (Lepidoptera: Noctuidae) e Spodoptera eridania – Spodoptera eridania (Cramer, 1782) (Lepidoptera: Noctuidae)',
-            d: 'Rachiplusia nu – Rachiplusia nu (Guenée) (Lepidoptera: Noctuidae) e Chrysodeixis includens – Chrysodeixis includens (Walker, 1858) (Lepidoptera: Noctuidae)',
+            a: <><i>Spodoptera eridania – Spodoptera eridania</i> (Cramer, 1782) (Lepidoptera: Noctuidae) e <i>Spodoptera cosmioides – Spodoptera cosmioides</i> (Walker, 1858) (Lepidoptera: Noctuidae)</>,
+            b: <><i>Chrysodeixis includens – Chrysodeixis includens</i> (Walker, 1858) (Lepidoptera: Noctuidae) e <i>Rachiplusia nu – Rachiplusia nu</i> (Guenée) (Lepidoptera: Noctuidae)</>,
+            c: <><i>Spodoptera cosmioides – Spodoptera cosmioides</i> (Walker, 1858) (Lepidoptera: Noctuidae) e <i>Spodoptera eridania – Spodoptera eridania</i> (Cramer, 1782) (Lepidoptera: Noctuidae)</>,
+            d: <><i>Rachiplusia nu – Rachiplusia nu</i> (Guenée) (Lepidoptera: Noctuidae) e <i>Chrysodeixis includens – Chrysodeixis includens</i> (Walker, 1858) (Lepidoptera: Noctuidae)</>,
             correta: 'd',
             respondido: false,
             imagens: {
@@ -313,16 +313,16 @@ function Perguntas() {
                         {pergunta[questao].respondido === false ? (
                             <div >
                                 <div className={respostaSelecionada === 'a' ? "respostas selecionada" : "respostas"} onClick={() => selecionarResposta('a')}>
-                                    <h6>a&#41; <i>{pergunta[questao].a}</i></h6>
+                                    <h6>a&#41; {pergunta[questao].a}</h6>
                                 </div>
                                 <div className={respostaSelecionada === 'b' ? "respostas selecionada" : "respostas"} onClick={() => selecionarResposta('b')}>
-                                    <h6>b&#41; <i>{pergunta[questao].b}</i></h6>
+                                    <h6>b&#41; {pergunta[questao].b}</h6>
                                 </div>
                                 <div className={respostaSelecionada === 'c' ? "respostas selecionada" : "respostas"} onClick={() => selecionarResposta('c')}>
-                                    <h6>c&#41; <i>{pergunta[questao].c}</i></h6>
+                                    <h6>c&#41; {pergunta[questao].c}</h6>
                                 </div>
                                 <div className={respostaSelecionada === 'd' ? "respostas selecionada" : "respostas"} onClick={() => selecionarResposta('d')}>
-                                    <h6>d&#41; <i>{pergunta[questao].d}</i></h6>
+                                    <h6>d&#41; {pergunta[questao].d}</h6>
                                 </div>
                             </div>
                         ) : (
