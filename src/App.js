3f1b43e3009560056eee7d355395125561aa7422
Nicoly001 <nicolyrod43@gmail.com>
2022-06-18 18:28:02 -0300

    bug deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes'
 
 function App() {
   return (
-    <Router>
       <Routes />
-    </Router>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import { Home, Perguntas, Resultado } from './pages'
 export default function MainRoutes() {
     return (
         <Routes>
-            <Route path="detetive-de-insetos/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/perguntas" element={<Perguntas />} />
             <Route path="/resultado" element={<Resultado />} />
         </Routes>
